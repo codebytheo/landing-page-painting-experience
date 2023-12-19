975a14b530005dccb3b00023f24f0c479551f976
codebytheo <theocode1@gmail.com>
2023-12-19 19:07:13 +0800

    init commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,141 @@
+'use client'
+
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+import { Pacifico } from 'next/font/google'
+
+const pasifico = Pacifico({ 
+  subsets: ['latin'],
+  weight: '400'
+})
 
 export default function Home() {
+  const bgAnimate = {
+    hidden:{
+      clipPath: "polygon(21% 26%, 77% 26%, 77% 77%, 21% 77%)",
+    },
+    show: {
+      clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      transition: {
+        ease:"easeInOut",
+        duration:0.8,
+        delay:1,
+      },
+    }
+  }
+  const textAnimate1 = {
+    hidden: {
+      y:"100%",
+      opacity:0,
+    },
+    show: {
+      y:0,
+      opacity:1,
+      transition: {
+        ease:'easeInOut',
+        duration:0.8,
+        staggerChildren:0.4,
+        delayChildren:1,
+      }
+    }
+  }
+  const textAnimate2 = {
+    hidden: {
+      x:0,
+    },
+    show: (i) => ({
+      x:i,
+      transition: {
+        ease: 'easeOut',
+        duration:0.8,
+      }
+    })
+  }
+  const imageAnimate = {
+    hidden:{ },
+    show: {
+      transition: {
+        staggerChildren: 0.6,
+        delayChildren:3.6,
+        ease: [0.25, 0.4, 0.25, 1],
+      }
+    }
+  }
+  const imageAnimateChild = {
+    hidden:{
+      opacity:0,
+      x:100,
+    },
+    show: {
+      opacity:1,
+      x:0,
+      transition: {
+        ease:'easeInOut'
+      }
+    }
+  }
+  const navAnimate = {
+    hidden:{
+      y:"-110%",
+    },
+    show: {
+      y:0,
+      transition: {
+        type: "spring",
+        stiffness: 60,
+        delay:2,
+      }
+    }
+  }
+  const textParagraph = {
+    hidden:{
+      y:"-100%",
+      opacity:0,
+    },
+    show: {
+      y:0,
+      opacity:1,
+      transition: {
+        type: "spring",
+        stiffness: 60,
+        delay:2.8,
+      }
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className='h-screen px-4 overflow-hidden'>
+      <motion.div className='absolute inset-0 w-screen h-screen z-0' variants={bgAnimate} initial="hidden" animate="show">
+        <Image src="/img/bg.webp" alt="background" fill sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw" priority={true} className='object-cover brightness-50' />
+      </motion.div>
+
+      <motion.nav className='flex justify-between items-center text-white relative z-10 pt-4' variants={navAnimate} initial="hidden" animate="show">
+        <div className={`text-xl text-yellow-200 font-bold underline ${pasifico.className}`}>BuildPainting</div>
+        <ul className='w-[300px] flex justify-between items-center'>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Destination</li>
+        </ul>
+      </motion.nav>
+      <div className='relative top-[120px]'>
+        <motion.div className='relative left-[25%]' variants={textAnimate1} initial="hidden" animate="show">
+          <motion.h1 className={`text-9xl text-[#eaeaea] lowercase tracking-tighter font-semibold z-10 ${pasifico.className}`} variants={textAnimate2} custom={-150}>PAINTING</motion.h1>
+        </motion.div>
+        <motion.p className='absolute top-12 right-32 z-10 w-[500px] text-justify leading-5 text-[#eaeaea] text-xs font-medium' variants={textParagraph} initial="hidden" animate="show"><span className='text-yellow-200'>The art of painting is a highly esteemed and revered form of artistic expression. It is a discipline that requires great skill, technique, and creativity.</span> Paintings have been created throughout history, serving as a means of communication, documentation, and aesthetic enjoyment.</motion.p>
+        <motion.div className='relative left-[25%]' variants={textAnimate1} initial="hidden" animate="show">
+          <motion.h1 className='text-9xl text-yellow-200 font-semibold tracking-tighter z-10' variants={textAnimate2} custom={100}>EXPERIENCE</motion.h1>
+        </motion.div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <motion.div className='flex gap-4 absolute bottom-4' variants={imageAnimate} initial="hidden" animate="show">
+        <motion.div className='relative w-[300px] h-[200px]' variants={imageAnimateChild}>
+          <Image src="/img/img-1.webp" alt="image" fill sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw" className='object-cover rounded-sm saturate-150' />
+        </motion.div>
+        <motion.div className='relative w-[300px] h-[200px]' variants={imageAnimateChild}>
+          <Image src="/img/img-2.webp" alt="image" fill sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw" className='object-cover rounded-sm saturate-150' />
+        </motion.div>
+        <motion.div className='relative w-[300px] h-[200px]' variants={imageAnimateChild}>
+          <Image src="/img/img-3.webp" alt="image" fill sizes="(max-width:768px) 33vw, (max-width:1024px) 50vw, 100vw" className='object-cover rounded-sm saturate-150' />
+        </motion.div>
+      </motion.div>
     </main>
   )
 }
